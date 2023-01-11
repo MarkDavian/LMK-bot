069516d4801c630f26ce4e6356567291e9174481
Alexander <nidillin@gmail.com>
2023-01-11 21:33:58 +0300

    CSV parser initial

---
+++ bot/core/data_parser/csv/parser.py
@@ -0,0 +1,11 @@
+from bot.core.utils.types.user import UserInfo
+from bot.core.utils.types.shedule import (
+    DayShedule,
+    WeekShedule, 
+    GroupShedule,  
+    WeekSheduleFactory, 
+    GroupSheduleFactory,
+)
+
+class CSVParser:
+    pass
