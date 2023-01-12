a1f39e1c2958ff3683569a2883789b2b07b11a7a
Alexander <nidillin@gmail.com>
2023-01-12 03:06:53 +0300

    Keep only metrics collector class

--- bot/core/statistics/collector/collector.py
+++ bot/core/statistics/collector/collector.py
@@ -1,28 +1,10 @@
 from config import settings
 
-
-class _MetricNameError(Exception):
-    def __init__(self, *args: object) -> None:
-        super().__init__(*args)
-
-    def __str__(self) -> str:
-        return super().__str__()
-
-
-class BaseMetricsStorage:
-    ...
-
-
-class CSVMetricsStorage(BaseMetricsStorage):
-    ...
-
-
-class MongoMetricsStorage(BaseMetricsStorage):
-    ...
-
-
-class TextMetricsStorage(BaseMetricsStorage):
-    ...
+from bot.core.statistics.collector.metrics_storage import (
+    _MetricNameError, 
+    MetricsStorageFactory, 
+    BaseMetricsStorage
+)
 
 
 class MetricsCollector:
@@ -30,7 +12,7 @@ class MetricsCollector:
 
     def __init__(self) -> None:
         self.metrics = settings.metrics['metrics']    
-        self.storage = settings.metrics['storage']
+        self.storage: BaseMetricsStorage = MetricsStorageFactory.get_storage(settings.metrics['storage'])
 
     def new_call(self, metric_name: str, *args) -> None:
         if metric_name not in self.metrics:
@@ -39,7 +21,7 @@ class MetricsCollector:
         self._save_metric(metric_name, args)
 
     def _save_metric(self, metric_name: str, *args) -> None:
-        ...
+        self.storage.save(metric_name, args)
 
     def new_metric(self, metric_name: str) -> None:
         self.metrics.append(metric_name)
