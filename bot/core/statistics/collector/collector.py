768adeb1668f2cfb780375713dac862141489210
Alexander <nidillin@gmail.com>
2023-01-13 02:03:32 +0300

    Fix *args

--- bot/core/statistics/collector/collector.py
+++ bot/core/statistics/collector/collector.py
@@ -18,8 +18,9 @@ class MetricsCollector:
         if metric_name not in self.metrics:
             raise _MetricNameError(f'Metric name ({metric_name}) is not defined')
 
-        self._save_metric(metric_name, args)
+        self._save_metric(metric_name, *args)
 
     def _save_metric(self, metric_name: str, *args) -> None:
-        self.storage.save(metric_name, args)
+        self.storage.save(metric_name, *args)
+
 
