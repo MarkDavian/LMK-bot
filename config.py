788fea1a028948df9ebb32f51b45a6cadef7d6b3
Alexander <nidillin@gmail.com>
2023-01-13 00:57:49 +0300

    Add settings_json, and timezone field

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+from datetime import timedelta, timezone
 import json
 from pydantic import BaseSettings
 
@@ -5,13 +6,22 @@ from pydantic import BaseSettings
 with open('settings/metrics.json', 'rb') as file:
     metrics_json = json.load(file)
 
+with open('settings/settings.json', 'rb') as file:
+    settings_json = json.load(file)
+
+
+_TZ_OFFSET = settings_json['timezone']['offset']
+_TZ_NAME = settings_json['timezone']['name']
+
 
 class Settings(BaseSettings):
     """Init of all settings
     """
     bot_api_key: str
     admin_id: int
+
     server_url: str
+    tz_info = timezone(timedelta(hours=_TZ_OFFSET), name=_TZ_NAME) 
 
     metrics = metrics_json
     metrics_filepath = 'database/metrics/metrics'
