5a1fd82fa95551520ee461b40bcfcb601fab27ee
Alexander <nidillin@gmail.com>
2023-01-13 01:42:53 +0300

    Update settings

--- config.py
+++ config.py
@@ -3,9 +3,6 @@ import json
 from pydantic import BaseSettings
 
 
-with open('settings/metrics.json', 'rb') as file:
-    metrics_json = json.load(file)
-
 with open('settings/settings.json', 'rb') as file:
     settings_json = json.load(file)
 
@@ -23,7 +20,7 @@ class Settings(BaseSettings):
     server_url: str
     tz_info = timezone(timedelta(hours=_TZ_OFFSET), name=_TZ_NAME) 
 
-    metrics = metrics_json
+    metrics = settings_json['metrics']
     metrics_filepath = 'database/metrics/metrics'
 
 
