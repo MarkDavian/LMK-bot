39436532e48daea0650ae64839dbf281e093bdb0
Alexander <nidillin@gmail.com>
2023-01-14 02:44:53 +0300

    Del MetricNameError

---
+++ bot/core/statistics/metrics/errors.py
@@ -0,0 +1,14 @@
+class _StorageTypeError(Exception):
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
+
+    def __str__(self) -> str:
+        return super().__str__()
+
+
+class _StorageFileError(Exception):
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
+
+    def __str__(self) -> str:
+        return super().__str__()
