26cf4693edfe650eb4752eef363caa66422942d7
Alexander <nidillin@gmail.com>
2023-01-14 02:47:46 +0300

    Update metrics

--- bot/core/statistics/proxy/proxy_shedule_db.py
+++ bot/core/statistics/proxy/proxy_shedule_db.py
@@ -3,16 +3,15 @@ from bot.core.utils.db.shedule import SheduleDB
 from bot.core.utils.types.user import UserInfo
 from bot.core.utils.types.shedule import GroupShedule, WeekShedule
 
-from bot.core.statistics.collector.collector import MetricsCollector
+from bot.core.statistics.metrics.metrics import metrics
 
 
 class CollectorProxy_SheduleDB:
     def __init__(self, shedDB: SheduleDB) -> None:
-        self.metrics = MetricsCollector()
         self.db = shedDB
 
     def get_shedule(self, userInfo: UserInfo) -> WeekShedule:
-        self.metrics.collect_new('Get shedule', userInfo.group, userInfo.place)
+        metrics.collect('Get shedule', userInfo.place, userInfo.group, userInfo.id, userInfo.social)
         return self.db.get_shedule(userInfo)
 
     def save_group_shedule(self, shedule: GroupShedule) -> None:
