4a728c994dbf689e4d173fdaee424a8b035b0a4e
Alexander <nidillin@gmail.com>
2023-01-14 22:26:22 +0300

    Update info

--- bot/core/utils/types/userinfo.py
+++ bot/core/utils/types/userinfo.py
@@ -10,6 +10,17 @@ class UserInfo:
     place: str
     social: str
 
-    def __init__(self, group: str, place: str) -> None:
+    def __init__(self, 
+                id: int, 
+                social: str, 
+                course: int, 
+                group: str, 
+                place: str) -> None:
+        self.id = id
+        self.course = course
         self.group = group
         self.place = place
+        self.social = social
+
+    def list(self):
+        return [self.place, self.group, self.id, self.social]
