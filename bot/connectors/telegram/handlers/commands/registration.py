5167fbf4f0466a7753b13f10c4b2ff94a5a469e9
Alexander <nidillin@gmail.com>
2023-01-14 22:27:14 +0300

    Register commands

--- bot/connectors/telegram/handlers/commands/registration.py
+++ bot/connectors/telegram/handlers/commands/registration.py
@@ -1,10 +1,13 @@
 from aiogram import Dispatcher, types
-
+from bot.connectors.telegram.handlers.commands.common import register_common_cmd
+from bot.connectors.telegram.handlers.commands.shedule import register_shedule_cmd
 
 
 async def register_all(dp: Dispatcher):
-    await dp.bot.set_my_commands(
-        [
-            types.BotCommand(""),
-        ]
-    )
+    # await dp.bot.set_my_commands(
+    #     [
+    #         types.BotCommand(""),
+    #     ]
+    # )
+    register_common_cmd(dp)
+    register_shedule_cmd(dp)
