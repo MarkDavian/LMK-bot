97bfd68a62deb5d9dceb85eee2cb877f012da560
Alexander <nidillin@gmail.com>
2023-01-15 17:50:20 +0300

    CSV beautifer added. URL or filepath added to pdf parser init args

--- bot/core/data_parser/PDFParser/pdf_parser.py
+++ bot/core/data_parser/PDFParser/pdf_parser.py
@@ -1,20 +1,54 @@
+from typing import Any
+import requests
+from urllib.parse import urlparse
+from pathlib import Path
+
 import tabula
 import pandas as pd
+from pydantic import BaseModel, validator
 
 from .csv_parser import CSVParser
+from .beautifer import CSVBeautifer
+
+
+class FileSrc(BaseModel):
+    path: str
+
+    @validator("path", pre=True)
+    def validate_src(cls, value: Any) -> Any:
+        url_parts = urlparse(value)
+        is_url = True
+        is_filepath = True
+
+        if url_parts.scheme and url_parts.scheme != "http":
+            is_url = False
+        elif not Path(url_parts.path).exists():
+            is_filepath = False
+
+        if not is_url and not is_filepath:
+            raise ValueError("src must be either existing local file path or HTTPS URL")
+
+        return value
 
 
 class PDFParser:
-    def __init__(self, filepath: str) -> None:
+    def __init__(self, src: str) -> None:
         """Read PDF in filepath and processes it converting to CSV cells
         and then to pandas DataFrame
 
         Args:
-            filepath (str): Path to PDF file
+            src (str): Path to PDF file or URL to file
         """
-        self.filepath = filepath
-        dfs = tabula.read_pdf(self.filepath, pages='all')
-        self.df = dfs[0]
+        src = FileSrc(path=src)
+        filepath = src.path
+        if self._is_url(filepath):
+            filepath = self._download_file(filepath)
+
+        output_csv = 'output.csv'
+
+        tabula.convert_into(filepath, output_csv, output_format="csv", pages='all')
+        self.df = CSVBeautifer(output_csv).beautify()
+
         self.parser = CSVParser(dataframe=self.df)
         self.parser.process()
 
@@ -39,3 +73,12 @@ class PDFParser:
     def extract_json(self, output: str) -> str:
         return self.parser.json(output=output)
 
+    def _is_url(self, filepath: str):
+        return True
+
+    def _download_file(self, filepath: str):
+        path = 'file.pdf'
+        with open(path, 'wb') as file:
+            r = requests.get(filepath)
+            file.write(r.content)
+        return path
