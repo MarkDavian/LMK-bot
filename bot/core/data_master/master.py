a6356ecb69d624c361f964c1812e810348a71e4c
Alexander <nidillin@gmail.com>
2023-01-16 03:06:13 +0300

    Minimal stable vers

--- bot/core/data_master/master.py
+++ bot/core/data_master/master.py
@@ -1,6 +1,11 @@
-from bot.core.scanner.site_parser import SiteParser
+import logging
+import json
+import time
+
+from bot.core.scanner.scanner import Scanner
 from bot.core.data_parser.PDFParser import PDFParser
 from bot.core.data_parser.JSONParser import JSONParser
+from bot.core.file_resolver.resolver import File
 from bot.core.utils.db.shedule import SheduleDB
 
 
@@ -13,6 +18,60 @@ DataOperator will observe for cached files, shedules to pretend over usage.
 """
 
 
+# master_logger = logging.getLogger(__name__)
+# master_logger.setLevel(logging.INFO)
+# handler = logging.FileHandler(f"DataMaster.log", mode='w')
+# formatter = logging.Formatter("%(name)s %(asctime)s %(levelname)s %(message)s")
+# handler.setFormatter(formatter)
+# master_logger.addHandler(handler)
+
+
 class DataMaster:
     def __init__(self) -> None:
-        ...
+        self.db = SheduleDB()
+        self.scanner = Scanner()
+        self.time_interval = self.scanner.interval
+
+    def start(self):
+        # try:
+        #     # master_logger.info('Data master started')
+        #     while True:
+        #         self._scan()
+        #         self._check()
+        #         # master_logger.info(f'Sleeping {self.time_interval}s')
+        #         time.sleep(self.time_interval)
+        # except Exception as er:
+        #     # master_logger.info('Data master stoped in cause of error')
+        #     # master_logger.error(er)
+
+        while True:
+            self._scan()
+            self._check()
+            # master_logger.info(f'Sleeping {self.time_interval}s')
+            time.sleep(self.time_interval)
+
+
+    def _scan(self):
+        # master_logger.info('Scanning site')
+        self.scanner.process()
+
+    def _check(self):
+        # master_logger.info('Checking file url')
+        if self.scanner.same_url:
+            pass
+        else:
+            self._save()
+
+    def _save(self):
+        # master_logger.info('Saving changes to DB')
+        jsonParser = JSONParser(PDFParser(src=self.scanner.result))
+        changeShedule = jsonParser.parse()
+        # with open(File('changes.json'), 'w') as file:
+        #     json.dump(changeShedule, file, ensure_ascii=False, indent=4, sort_keys=True)
+        self.db.save_change_shedule(change=changeShedule)
+
+
+def start_data_master():
+    # master_logger.info('Starting data master')
+    master = DataMaster()
+    master.start()
