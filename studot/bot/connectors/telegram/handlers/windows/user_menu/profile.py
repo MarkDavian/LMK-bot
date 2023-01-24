5054863efefc29778edc719a2c3f49956c657f03
Alexander <nidillin@gmail.com>
2023-01-24 21:46:48 +0300

    .

--- studot/bot/connectors/telegram/handlers/windows/user_menu/profile.py
+++
@@ -1,9 +0,0 @@
-from ..common import *
-
-profile = Window(
-        Const('Профиль'),
-        SwitchTo(Const('Сменить группу'), id='inline_start', state=UserMenuSG.inline_step_one),
-        SwitchTo(Const('Назад'), id='back', state=UserMenuSG.start),
-        state=UserMenuSG.profile_start,
-        parse_mode=ParseMode.HTML
-    )
