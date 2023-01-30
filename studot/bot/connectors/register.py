8d783a477d5e7dc5b624bf92d8203793dac22b8c
Alexander <nidillin@gmail.com>
2023-01-31 01:56:50 +0300

    Multiprocessing

--- studot/bot/connectors/register.py
+++ studot/bot/connectors/register.py
@@ -1,9 +1,21 @@
-from bot.connectors.telegram.registry import start_tg
+import multiprocessing as mp
 
-from bot.connectors.vk import *
+from bot.connectors.telegram.registry import run_tg_bot
+from bot.connectors.vk.registry import run_vk_bot
 
 
-async def start_connectors():
+def start_connectors():
     """Start polling bots
     """
-    await start_tg()
+    run_vk()
+    run_tg()
+
+
+def run_vk():
+    vk_bot_proc = mp.Process(target=run_vk_bot)
+    vk_bot_proc.start()
+
+
+def run_tg():
+    tg_bot_proc = mp.Process(target=run_tg_bot)
+    tg_bot_proc.start()
