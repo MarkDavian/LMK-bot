82921272431a2e27a6d60d61c1d4391823133f15
Alexander <nidillin@gmail.com>
2023-01-31 01:58:22 +0300

    Start bot and user registration

---
+++ studot/bot/connectors/vk/commands/common.py
@@ -0,0 +1,80 @@
+from vkbottle import BaseStateGroup, Keyboard, EMPTY_KEYBOARD, Text, KeyboardButtonColor
+from vkbottle.bot import Message
+from vkbottle.dispatch.rules.base import CommandRule
+
+from bot.connectors.vk.vk_bot_config import labeler, state_dispenser
+
+from bot.core.statistics.proxy.proxy_users_db import usersDB
+
+from bot.core.utils.types.userinfo import UserInfo
+
+
+def get_user_info(user_id: int) -> UserInfo:
+    userInfo = usersDB.get_user_info(user_id)
+
+    return userInfo
+
+
+class RegistrationSG(BaseStateGroup):
+    start = 'start'
+    groupInput = 'groupInput'
+    courseInput = 'courseInput'
+
+
+@labeler.message(CommandRule('начать', ['/', '!']))
+async def cmd_start(message: Message):
+    # Remove user state
+    if peer := await state_dispenser.get(message.peer_id):
+        await state_dispenser.delete(peer.peer_id)
+
+    userInfo = get_user_info(message.from_id)
+    
+    if userInfo is not None:
+        return
+    else:
+        await message.answer(
+            ("Я -- бот для расписания)\n"+
+            "Укажи свою группу, например ПИ 19-2"),
+            keyboard=EMPTY_KEYBOARD
+        )
+        await state_dispenser.set(message.peer_id, RegistrationSG.groupInput)
+
+
+@labeler.message(text='<group>', state=RegistrationSG.groupInput)
+async def group_input(message: Message, group: str):
+    keyboard = (
+        Keyboard()
+        .add(Text('1'))
+        .add(Text('2'))
+        .row()
+        .add(Text('3'))
+        .add(Text('4'))
+    )
+
+    await message.answer(
+        "Отлично, теперь укажи свой курс",
+        keyboard=keyboard
+    )
+    await state_dispenser.set(message.peer_id, RegistrationSG.courseInput, group=group)
+
+
+@labeler.message(text='<course>', state=RegistrationSG.courseInput)
+async def course_input(message: Message, course: str):
+    group = message.state_peer.payload['group']
+
+    userInfo = UserInfo(
+        userID=message.from_id,
+        social='vk',
+        course=course,
+        group=group,
+        place='ЛМК'
+    )
+    usersDB.create_user(userInfo)
+    await message.answer(
+        "Успешно зарегистрировал тебя! Теперь ты можешь воспользоваться /menu )",
+        keyboard=EMPTY_KEYBOARD
+    )
+
+    # Remove user state
+    await state_dispenser.delete(message.peer_id)
+
