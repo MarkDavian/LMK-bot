383d7d62f7806d163c62d4d6e8156274698575fc
Alexander <nidillin@gmail.com>
2023-01-31 01:58:11 +0300

    Vk bot init

---
+++ studot/bot/connectors/vk/vk_bot.py
@@ -0,0 +1,15 @@
+from vkbottle import Bot
+
+from config import settings
+
+from bot.connectors.vk.vk_bot_config import labeler, state_dispenser
+import bot.connectors.vk.commands.common
+import bot.connectors.vk.menu.shedule
+import bot.connectors.vk.menu.start_menu
+import bot.connectors.vk.menu.settings
+import bot.connectors.vk.menu.change_group
+import bot.connectors.vk.menu.additional
+
+
+bot = Bot(token=settings.vk_bot_api_key, labeler=labeler, state_dispenser=state_dispenser)
+
