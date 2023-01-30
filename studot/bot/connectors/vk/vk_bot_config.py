02ef085fb9e592da2d5732063babafe7f1240e1b
Alexander <nidillin@gmail.com>
2023-01-31 01:58:00 +0300

    Global vk bot config

---
+++ studot/bot/connectors/vk/vk_bot_config.py
@@ -0,0 +1,7 @@
+from vkbottle import BuiltinStateDispenser
+from vkbottle.bot import BotLabeler
+
+
+labeler = BotLabeler()
+labeler.vbml_ignore_case = True
+state_dispenser = BuiltinStateDispenser()
