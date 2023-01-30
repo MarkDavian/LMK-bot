06aada9bdd7e748485e3c3a4e9af606656d5480f
Alexander <nidillin@gmail.com>
2023-01-31 01:58:37 +0300

    Additional shedule menu

---
+++ studot/bot/connectors/vk/menu/additional.py
@@ -0,0 +1,23 @@
+from vkbottle import Keyboard, Text, KeyboardButtonColor
+from vkbottle.bot import Message
+
+from bot.connectors.vk.vk_bot_config import labeler, state_dispenser
+
+from bot.connectors.vk.menu.start_menu import MenuSG
+
+
+@labeler.message(text='дополнительно', state=MenuSG.start)
+@labeler.message(text='назад', state=MenuSG.menuDayShedule)
+async def menu_additional(message: Message):
+    await state_dispenser.set(message.peer_id, MenuSG.additional)
+    keyboard = (
+        Keyboard()
+        .add(Text('Расписание на день')).row()
+        .add(Text('Расписание звонков')).row()
+        .add(Text('Цвет недели')).row()
+        .add(Text('Назад'), KeyboardButtonColor.PRIMARY)
+    )
+    await message.answer(
+        'Дополнительно',
+        keyboard=keyboard
+    )
