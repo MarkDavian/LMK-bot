2f39c40679054796f987f1ce4538d3e5d223206c
Alexander <nidillin@gmail.com>
2023-01-31 20:35:23 +0300

    Add profile labeler

--- studot/bot/connectors/vk/vk_bot.py
+++ studot/bot/connectors/vk/vk_bot.py
@@ -9,6 +9,7 @@ import bot.connectors.vk.menu.start_menu
 import bot.connectors.vk.menu.settings
 import bot.connectors.vk.menu.change_group
 import bot.connectors.vk.menu.additional
+import bot.connectors.vk.menu.profile
 
 
 bot = Bot(token=settings.vk_bot_api_key, labeler=labeler, state_dispenser=state_dispenser)
