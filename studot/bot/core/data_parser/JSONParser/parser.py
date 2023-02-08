23e84f1c5d11d6179bdcf921c7705b7b1f458cf1
Alexander <nidillin@gmail.com>
2023-02-08 12:39:09 +0300

    CAPS groups names and sorted subjects

--- studot/bot/core/data_parser/JSONParser/parser.py
+++ studot/bot/core/data_parser/JSONParser/parser.py
@@ -31,7 +31,8 @@ class JSONParser:
 
         for course, shedule in doc['Курс'].items():
             for group_name, shed in shedule.items():
-                for key, subject in shed.items():
+                group_name = group_name.capitalize()
+                for key, subject in sorted(shed.items()):
                     time = SHEDULE_TIME.SUBJECTS[int(key[-1])-1]
                     if isinstance(subject, float):
                         subject = ""
